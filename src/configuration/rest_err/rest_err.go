b9a1bbefad494be0b5edb37baeacd2789a5ba66a
Marlliton Souza <marlliton.souza1@gmail.com>
2024-10-07 15:52:25 -0300

    Criando manipuladores de erros

---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,69 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"err"` // Error Type
+	Code    int      `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Feild   string `json:"feild"`
+	Message string `json:"message"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestValidationErr(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewNotForbiddenErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
+
+func NewNotFoundErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewInternalServerErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
