9afac09d0ca54741b8b22fadac31a08022caee01
Marlliton Souza <marlliton.souza1@gmail.com>
2024-10-08 14:25:41 -0300

    Criando um pacote de logs

---
+++ src/configuration/logger/logger.go
@@ -0,0 +1,70 @@
+package logger
+
+import (
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log *zap.Logger
+)
+
+const (
+	LOG_LEVEL  = "LOG_LEVEL"
+	LOG_OUTPUT = "LOG_OUTPUT"
+)
+
+func init() { // a função "init" é chamada sempre que o pacote é carregado
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutputLogs()},
+		Level:       zap.NewAtomicLevelAt(getLevelLogs()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "message",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	log, _ = logConfig.Build()
+}
+
+func Info(msg string, tags ...zapcore.Field) {
+	log.Info(msg, tags...)
+	log.Sync()
+}
+
+func Error(msg string, err error, tags ...zapcore.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Info(msg, tags...)
+	log.Sync()
+}
+
+func getLevelLogs() zapcore.Level {
+	switch strings.ToLower(strings.TrimSpace(os.Getenv(LOG_LEVEL))) {
+	case "info":
+		return zapcore.InfoLevel
+	case "error":
+		return zapcore.ErrorLevel
+	case "debug":
+		return zapcore.DebugLevel
+	default:
+		return zapcore.InfoLevel
+	}
+
+}
+
+func getOutputLogs() string {
+	output := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_OUTPUT)))
+	if output == "" {
+		return "stdout"
+	}
+
+	return output
+}
